aeaa68d7229a7326b53f8caa6c1fa7bc1688cbcb
sok thin <phabsokthin992003@gmail.com>
2026-03-29 17:53:17 +0700

    update card and remove app screen

---
+++ src/components/Card.tsx
@@ -0,0 +1,36 @@
+function Card() {
+  return (
+    <div className="">
+      <div
+        style={{
+          background: "yellow",
+          padding: "10px",
+          border: "1px solid red",
+        }}
+      >
+        Card Component For New Version
+      </div>
+      <div
+        style={{
+          background: "green",
+          padding: "10px",
+          border: "1px solid red",
+        }}
+      >
+        Card Component For New Version
+      </div>
+
+      <div
+        style={{
+          background: "orange",
+          padding: "10px",
+          border: "1px solid red",
+        }}
+      >
+        Card Component For New Version
+      </div>
+    </div>
+  );
+}
+
+export default Card;
