e27b984b1fd15c95b567c1bdd7eeb8bd73bb3812
sok thin <phabsokthin992003@gmail.com>
2026-03-29 17:37:31 +0700

    add vercell and update

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,8 +3,8 @@ function Button() {
   return (
     <>
     
-    <div style={{ background: "red", padding: "10px" }}>Button Button Component</div>
-    <div style={{ background: "blue", padding: "10px" }}>Button Button Component</div>
+    <div style={{ background: "red", padding: "10px" }}>Button Button Componentss</div>
+    <div style={{ background: "blue", padding: "10px" }}>Button Button Componentsss</div>
     
     </>
   )
