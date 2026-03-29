1e4a329c1587a751ced48f80daeb68c4fa013bca
sok thin <phabsokthin992003@gmail.com>
2026-03-29 17:41:14 +0700

    update button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,8 +3,8 @@ function Button() {
   return (
     <>
     
-    <div style={{ background: "red", padding: "10px" }}>Button Button Componentss</div>
-    <div style={{ background: "blue", padding: "10px" }}>Button Button Componentsss</div>
+    <div style={{ background: "red", padding: "10px" }}>Button Button Componentss TEsting</div>
+    <div style={{ background: "blue", padding: "10px" }}>Button Button Componentsss TEsting</div>
     
     </>
   )
