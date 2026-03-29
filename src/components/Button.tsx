589d57f6326f97712ed2a6d4033dce2675b0ebaf
sok thin <phabsokthin992003@gmail.com>
2026-03-29 17:15:33 +0700

    update button blue and workflow

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,12 @@
 
 function Button() {
   return (
+    <>
+    
     <div style={{ background: "red", padding: "10px" }}>Button Button Component</div>
+    <div style={{ background: "blue", padding: "10px" }}>Button Button Component</div>
+    
+    </>
   )
 }
 
